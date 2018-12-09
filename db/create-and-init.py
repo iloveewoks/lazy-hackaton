3e0998a8ef580d902dadd9a85feab05ffa04ee6a
Dmitriy Kapitun <kapchook@gmail.com>
2018-12-10 01:45:47 +0300

    Add risk_points collection

--- db/create-and-init.py
+++ db/create-and-init.py
@@ -4,10 +4,20 @@ import json
 client = pymongo.MongoClient("mongodb://localhost:27017")
 
 db = client["lazy"]
-buildings = db["buildings"]
-buildings.drop()
+places = db["places"]
+risk_points = db["risk_points"]
+places.drop()
+risk_points.drop()
+
+with open('dangerous_object.geojson') as f:
+	dan_obj_data = json.load(f)
+
+risk_points.insert_many(dan_obj_data['features'])
+point = dan_obj_data['features'][0]
 
 with open('buildings.geojson') as f:
-	data = json.load(f)
+	places_data = json.load(f)
+	places_data['features'][0]['restricted_by'] = { 'UID': point['properties']['UID'], 'RESTRICTION': 'red' }
 
-buildings.insert_many(data['features'])
+places.insert_many(places_data['features'])
+
